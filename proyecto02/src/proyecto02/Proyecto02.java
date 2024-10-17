b8b911eef8e040ee4155d39a96ca674c312cb87a
Emilio Jose C <ecueva611@gmail.com>
2024-10-17 11:21:33 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author utpl
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        System.out.println("Ejemplo dos básico de Java versionado a través de Git manejado desde GitHub, además se usar GitHubClassroom");// TODO code application logic here
+    }
+    
+}
